4013936bf7744be59d4bbcbf1cb85cd88f32a0e7
jshtengker <leonardotengker@gmail.com>
2023-10-02 21:41:07 +0800

    adding another files

---
+++ Exercise1.js
@@ -0,0 +1,12 @@
+
+function isNumber(number){
+    if (number < 0){
+        console.log(number, 'bilangan negatif');
+    }
+    else{
+        console.log(number, 'bilangan positif');
+    }      
+}
+
+// isNumber(5);
+// isNumber(-5);
